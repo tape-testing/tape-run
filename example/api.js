8ca97d2d0d9fa468db9ec7b1d744a513553bc5d8
Julian Gruber <julian@juliangruber.com>
2016-01-09 20:26:42 +0100

    example

--- example/api.js
+++ example/api.js
@@ -1,7 +1,11 @@
 var run = require('..');
 var browserify = require('browserify');
+var resolve = require('path').resolve;
 
-browserify(__dirname + '/../test/fixtures/one.js')
+var script = process.argv[2] || __dirname + '/../test/fixtures/one.js';
+script = resolve(script);
+
+browserify(script)
   .bundle()
   .pipe(run())
   .on('results', console.log)
