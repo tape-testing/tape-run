32d2021ba9ddfd8d737ae2ab2a045b5281070956
Julian Gruber <julian@juliangruber.com>
2022-05-04 08:06:53 +0200

    replace deprecated optimist with yargs

--- bin/run.js
+++ bin/run.js
@@ -1,53 +1,60 @@
 #!/usr/bin/env node
 
 var run = require('..');
-var optimist = require('optimist');
+var yargs = require('yargs/yargs');
+var { hideBin } = require('yargs/helpers');
 var spawn = require('child_process').spawn;
 
-var argv = optimist
+var argv = yargs(hideBin(process.argv))
   .usage('Pipe a browserify stream into this.\nbrowserify [opts] [files] | $0 [opts]')
 
-  .describe('wait', 'Timeout for tap-finished')
-  .alias('w', 'wait')
-
-  .describe('port', 'Wait to be opened by a browser on that port')
-  .alias('p', 'port')
-
-  .describe('static', 'Serve static files from this directory')
-  .alias('s', 'static')
-
-  .describe('browser', 'Browser to use. ' +
-      'Always available: electron. ' +
-      'Available if installed: chrome, firefox, ie, phantom, safari')
-  .alias('b', 'browser')
-  .default('browser', 'electron')
-
-  .describe('render', 'Command to pipe tap output to for custom rendering')
-  .alias('r', 'render')
-
-  .describe('keep-open', 'Leave the browser open for debugging after running tests')
-  .alias('k', 'keep-open')
-  .alias('keepOpen', 'keep-open')
-
-  .describe('node', 'Enable nodejs integration for electron')
-  .alias('n', 'integration')
-  .alias('node-integration', 'node')
-  .alias('nodeIntegration', 'node')
-
-  .describe('sandbox', 'Enable electron sandbox')
-  .boolean('sandbox')
-  .default('sandbox', true)
-
-  .describe('basedir', 'Set this if you need to require node modules in node mode')
-
-  .describe('help', 'Print usage instructions')
-  .alias('h', 'help')
-
-  .argv;
-
-if (argv.help) {
-  return optimist.showHelp();
-}
+  .option('wait', {
+    alias: 'w',
+    type: 'number',
+    description: 'Timeout for tap-finished'
+  })
+  .option('port', {
+    alias: 'p',
+    type: 'number',
+    description: 'Wait to be opened by a browser on that port'
+  })
+  .option('static', {
+    alias: 's',
+    type: 'string',
+    description: 'Serve static files from this directory'
+  })
+  .option('browser', {
+    alias: 'b',
+    type: 'string',
+    default: 'electron',
+    description: 'Browser to use. ' +
+    'Always available: electron. ' +
+    'Available if installed: chrome, firefox, ie, phantom, safari'
+  })
+  .option('render', {
+    alias: 'r',
+    type: 'string',
+    description: 'Command to pipe tap output to for custom rendering'
+  })
+  .option('keep-open', {
+    alias: ['k', 'keepOpen'],
+    type: 'boolean',
+    description: 'Leave the browser open for debugging after running tests'
+  })
+  .option('node', {
+    alias: ['n', 'node-integration', 'nodeIntegration'],
+    type: 'boolean',
+    description: 'Enable nodejs integration for electron'
+  })
+  .option('sandbox', {
+    type: 'boolean',
+    default: true,
+    description: 'Enable electron sandbox'
+  })
+  .option('basedir', {
+    description: 'Set this if you need to require node modules in node mode'
+  })
+  .parse();
 
 var runner = run(argv);
 
